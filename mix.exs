f4752701af99b71c80476ce58961e4dcb303b5b3
Victor Borja <vborja@apache.org>
2017-01-13 01:51:14 -0600

    since elixir 1.4

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Spec.Mixfile do
   def project do
     [app: :spec,
      version: "0.1.0",
-     elixir: "~> 1.2",
+     elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps()]
