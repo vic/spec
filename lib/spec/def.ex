4c174f6ab26211897f639edd6bb00ed7ad9d1927
Victor Borja <vborja@apache.org>
2017-01-13 23:12:24 -0600

    add defspecp

--- lib/spec/def.ex
+++ lib/spec/def.ex
@@ -1,13 +1,24 @@
 defmodule Spec.Def do
 
   @spec defspec(Macro.t) :: Macro.t
-  defmacro defspec({name, _, [quoted_expr]}) do
-    conformer = Spec.Quoted.conformer(quoted_expr)
-    unformer = Spec.Quoted.conformer(quote do: fn x -> x end)
+  defmacro defspec({name, _, [conformer]}) do
+    unformer = quote do: fn x -> x end
+    define(:def, name, conformer, unformer)
+  end
+
+  @spec defspec(Macro.t) :: Macro.t
+  defmacro defspecp({name, _, [conformer]}) do
+    unformer = quote do: fn x -> x end
+    define(:defp, name, conformer, unformer)
+  end
+
+  defp define(def, name, conformer, unformer) do
+    conformer = Spec.Quoted.conformer(conformer)
+    unformer = Spec.Quoted.conformer(unformer)
     quote do
 
       @spec unquote(name)() :: Spec.Transformer.t
-      def unquote(name)() do
+      unquote(def)(unquote(name)()) do
         %Spec.Transform{
           conformer: unquote(conformer),
           unformer: unquote(unformer)
@@ -15,22 +26,22 @@ defmodule Spec.Def do
       end
 
       @spec unquote(name)(any) :: Spec.Conformer.result
-      def unquote(name)(value) do
+      unquote(def)(unquote(name)(value)) do
         Spec.Transformer.conform(unquote(name)(), value)
       end
 
       @spec unquote(name)(any) :: boolean
-      def unquote(:"#{name}?")(value) do
+      unquote(def)(unquote(:"#{name}?")(value)) do
         unquote(name)(value) |> Spec.Kernel.ok?
       end
 
       @spec unquote(name)(any) :: any
-      def unquote(:"#{name}!")(value) do
+      unquote(def)(unquote(:"#{name}!")(value)) do
         unquote(name)(value)
         |> case do
-          {:ok, conformed} -> conformed
-          {:error, mismatch = %Spec.Mismatch{}} -> raise mismatch
-        end
+             {:ok, conformed} -> conformed
+             {:error, mismatch = %Spec.Mismatch{}} -> raise mismatch
+           end
       end
     end
   end
